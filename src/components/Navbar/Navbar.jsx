de058f264dca6d747f4594d3747b758c6a64df34
Raaymii Oghagbon <oghomwenoghagbon@gmail.com>
2022-11-14 11:46:41 +0100

    Created a clr jsx and scss files within the folder

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../assets/images/svg/logo.svg";
+import "../Navbar/Navbar.scss";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <ul className="main-list">
+          <li>
+            <img src={logo} className="logo"></img>
+          </li>
+          <li>Save</li>
+          <li>Invest</li>
+          <li>Stories</li>
+          <li>FAQs</li>
+          <li>Blog</li>
+          <li>
+            <button> Sign in</button>
+          </li>
+          <li>
+            <button>Create free account</button>
+          </li>
+          <div className="mobile-nav">
+            <ion-icon name="menu"></ion-icon>
+          </div>
+        </ul>
+      </nav>
+    </>
+  );
+}
