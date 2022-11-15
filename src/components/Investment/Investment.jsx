09c8197d59d4e63cf5b742935a0f760b1b2839f9
Raaymii Oghagbon <oghomwenoghagbon@gmail.com>
2022-11-15 11:38:44 +0100

    Added an Investment section

---
+++ src/components/Investment/Investment.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../Investment/Investment.scss";
+import circleinvesticon from "../../assets/images/svg/investicon.svg";
+import dotinvesticon from "../../assets/images/svg/dotinvesticon.svg";
+import phoneinvest from "../../assets/images/png/invest-phone.png";
+
+export default function Investment() {
+  return (
+    <div>
+      <section className="section-investment">
+        <div className="investment-container">
+          <div className="investment-image">
+            <img src={circleinvesticon} alt="" className="circleinvesticon" />
+            <img src={phoneinvest} alt="" className="phoneinvest" />
+            <img src={dotinvesticon} alt="" className="dotinvesticon" />
+          </div>
+          <div className="investment-writeup">
+            <div className="bonus">
+              <p>Up to 25% Returns</p>
+            </div>
+            <h2>Access Investment Opportunities</h2>
+            <p className="invest-p">
+              Invest securely and confidently on the go. Grow your money
+              confidently by investing in pre-vetted investment opportunities.
+            </p>
+            <a href="#">Learn more about Investments &#62;</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
