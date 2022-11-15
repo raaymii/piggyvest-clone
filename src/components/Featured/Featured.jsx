c4bb809d82bbec3b965c2fd73d49205f20b156aa
Raaymii Oghagbon <oghomwenoghagbon@gmail.com>
2022-11-15 11:38:04 +0100

    Added a featured section

---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Featured/Featured.scss";
+import techpoint from "../../assets/images/png/techpoint.png";
+import theGuardian from "../../assets/images/png/theguardian.png";
+import techcrunch from "../../assets/images/png/techcrunch.png";
+
+export default function Featured() {
+  return (
+    <div>
+      <section className="section-featured">
+        <div className="featured-heading">
+          <h3>As featured in</h3>
+        </div>
+        <div className="featured-in">
+          <img src={techpoint} alt="" className="featured-images" />
+          <img src={theGuardian} alt="" className="featured-images" />
+          <img src={techcrunch} alt="" className="featured-images" />
+        </div>
+      </section>
+    </div>
+  );
+}
