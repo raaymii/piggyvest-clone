4385d0f0ac70c445cd9bf8a8db29703f791ee57d
Raaymii Oghagbon <oghomwenoghagbon@gmail.com>
2022-11-15 11:37:21 +0100

    Added a Customers section

---
+++ src/components/Customers/Customers.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../Customers/Customers.scss";
+
+export default function Customers() {
+  return (
+    <div>
+      <section className="section-customers">
+        <div className="animation-testimonials"></div>
+        <div className="customers">
+          <h3>4 Million+ customers</h3>
+          <p>
+            Since launching in 2016, over 4,000,000 people have used PiggyVest
+            to manage their money better, avoid over-spending and be more
+            accountable.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
