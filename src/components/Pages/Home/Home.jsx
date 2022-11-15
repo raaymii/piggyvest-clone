67f7118486b11eb8a0640451e1b53f8d7b23b516
Raaymii Oghagbon <oghomwenoghagbon@gmail.com>
2022-11-15 11:43:07 +0100

    Added more components to the Home component

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -3,6 +3,11 @@ import Header from "../../Header/Header";
 import Navbar from "../../Navbar/Navbar";
 import Security from "../../Security/Security";
 import Savings from "../../Savings/Savings";
+import Investment from "../../Investment/Investment";
+import Saver from "../../Saver/Saver";
+import Customers from "../../Customers/Customers";
+import Featured from "../../Featured/Featured";
+import Footer from "../../Footer/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +16,11 @@ export default function Home() {
       <Header />
       <Security />
       <Savings />
+      <Investment />
+      <Saver />
+      <Customers />
+      <Featured />
+      <Footer />
     </div>
   );
 }
